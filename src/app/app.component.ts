4fd82aeb6ec3672bd0a2f3703ac71237c0f4e614
Maria G Moreno R <guadalupemoreno@uadec.edu.mx>
2023-09-29 23:06:28 -0600

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
-
+import {  FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularEjemplo';
+  contactForm: FormGroup; // Reactive form group
+
+  constructor(private formBuilder: FormBuilder) {
+    // inicializar los contro
+    this.contactForm = this.formBuilder.group({
+      representante: ['', Validators.required], 
+      curp: ['', Validators.required],
+      nombre: ['', Validators.required],
+      paterno: ['', Validators.required],
+      materno: ['', Validators.required],
+      telefono: ['', Validators.required],
+      celular: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      nivel: ['', Validators.required], 
+      municipio: ['', Validators.required],
+      asunto: ['', Validators.required],
+    });
+  }
+
+
+  onSubmit() {
+    this.markFormGroupTouched(this.contactForm);
+
+  if (this.contactForm.valid) {
+
+    console.log(this.contactForm.value);
+  }
+}
+
+  // Helper function to mark all controls as touched
+  private markFormGroupTouched(formGroup: FormGroup) {
+    Object.values(formGroup.controls).forEach(control => {
+ 
+      if (control instanceof FormGroup) {
+        this.markFormGroupTouched(control);
+      }else{
+        control.markAsTouched();
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
